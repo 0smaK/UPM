4b0e35cb63fa2dd49f60872cdc45689be5d28596
Óscar Muñoz <oscarmc99@gmail.com>
2017-11-23 23:58:08 +0100

    Update enunciado 1.c

--- Taller de programación/Practica 10/enunciado 1.c
+++ Taller de programación/Practica 10/enunciado 1.c
@@ -1,9 +1,11 @@
 //TO-DO
 /*
-* Se leen desde un fichero de texto (ENT.TXT) una secuencia de valores indeterminada de números enteros, como máximo 20. Escribir un programa que grabe en otro fichero de texto (SAL.TXT) solo aquellos valores que han sido leídos y que sea múltiplos de 5.
-* El fichero de texto ENT.TXT debe ser creado previamente con block de notas.
-* El fichero de texto SAL.TXT si existe previamente se perderá.
-*/
+ * Se leen desde un fichero de texto (ENT.TXT) una secuencia de valores indeterminada de números enteros,
+ * como máximo 20. Escribir un programa que grabe en otro fichero de texto (SAL.TXT)
+ * solo aquellos valores que han sido leídos y que sea múltiplos de 5.
+ * El fichero de texto ENT.TXT debe ser creado previamente con block de notas.
+ * El fichero de texto SAL.TXT si existe previamente se perderá.
+ */
 #include <stdio.h>
 
 //prototipos
@@ -11,5 +13,14 @@ void leer   (FILE *pf1, int v[], int *longitud);
 void grabar (int v[], int longitud,FILE *pf2);
 
 int main(){
+        FILE *pf1;
+        FILE *pf2;
+        int v[20];
+        int *logitud;
 
+        leer(*pf1,v,*longitud){
+              *pf1 = fopen("ENT.TXT","r");
+              fscanf("")
+
+        }
 }
