3e3fca0d011fb119b73edef81761b3fa4dfaa7fd
Óscar Muñoz <oscarmc99@gmail.com>
2017-11-23 21:45:32 +0100

    Create "enunciado 1.c"

---
+++ Taller de programación/Practica 10/enunciado 1.c
@@ -0,0 +1,15 @@
+//TO-DO
+/*
+* Se leen desde un fichero de texto (ENT.TXT) una secuencia de valores indeterminada de números enteros, como máximo 20. Escribir un programa que grabe en otro fichero de texto (SAL.TXT) solo aquellos valores que han sido leídos y que sea múltiplos de 5.
+* El fichero de texto ENT.TXT debe ser creado previamente con block de notas.
+* El fichero de texto SAL.TXT si existe previamente se perderá.
+*/
+#include <stdio.h>
+
+//prototipos
+void leer   (FILE *pf1, int v[], int *longitud);
+void grabar (int v[], int longitud,FILE *pf2);
+
+int main(){
+
+}
