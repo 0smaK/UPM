26259e3afc80f24ff04e579dac196ed1620cdea1
Óscar Muñoz <oscarmc99@gmail.com>
2017-11-27 19:39:32 +0100

    Finished enunciado 1.c

--- Taller de programación/Practica 10/enunciado 1.c
+++ Taller de programación/Practica 10/enunciado 1.c
@@ -1,6 +1,5 @@
-//TO-DO
 /*
- * Se leen desde un fichero de texto (ENT.TXT) una secuencia de valores indeterminada de números enteros,
+ * Se leen desde un fichero de texto (ENT.TXT) una secuencia de valores indeterminada de nímeros enteros,
  * como máximo 20. Escribir un programa que grabe en otro fichero de texto (SAL.TXT)
  * solo aquellos valores que han sido leídos y que sea múltiplos de 5.
  * El fichero de texto ENT.TXT debe ser creado previamente con block de notas.
@@ -14,33 +13,33 @@ void grabar(int v[], int longitud,FILE *pf2);
 
 int main(){
         FILE *pf1;
-		FILE *pf2;
+        FILE *pf2;
+
         int v[20];
-        int longitud = 0;
+        int longitud;
 
         leer(pf1,v,&longitud);
-	grabar(v,longitud,pf2);
-        
+        grabar(v,longitud,pf2);
+
         return 0;
 }
-void leer(FILE *pf1, int v[], int *longitud){	
-	pf1 = fopen("ENT.TXT","r");
-	int i,valor;
-    while((fscanf(pf1,"%d",&valor))!=EOF){
-	    if((valor%5)==0){
-		  	(*longitud)++;
-	    	v[i] = valor;
-			i++;
-		}
-	}
-	fclose(pf1);
+
+void leer(FILE *pf1, int v[], int *longitud){
+
+        pf1 = fopen("ENT.TXT","r"); //Abrimos pf1 en modo lectura
+        int i=0,valor; //definimos dos variables
+        while(((fscanf(pf1,"%d",&v[i]))!=EOF)&&(i<20)) //ejecutar el siguiente bloque hasta EOF o i<20
+                i++;
+        *longitud=i;
+        fclose(pf1); //Cerramos fichero
 }
 
 void grabar(int v[], int longitud,FILE *pf2){
-	pf2 = fopen("SAL.TXT","w");
-	int i;
-	for(i=0;i<longitud;i++){
-		fprintf(pf2,"%d\n",v[i]);
-	}	
-	fclose(pf2);
+        int i;
+        pf2 = fopen("SAL.TXT","w"); //Abrimos pf2 en modo escritura
+        for(i=0; i<longitud; i++) { //hasta que i<longitud del array imprimir numeros en pf2
+                if((v[i]%5)==0) //Si el valor es multiplo de 5 ejecutar lo siguientefprintf(pf2,"%d\n",v[i]);
+                        fprintf(pf2,"%d\n",v[i]);
+        }
+        fclose(pf2);//Cerramos pf2
 }
